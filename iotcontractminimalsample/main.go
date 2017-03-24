3bbf2e6cb2db444149c42d76daacf1e8bdaf627d
Sayantan <satarafd@in.ibm.com>
2017-03-24 16:13:08 +0530

    fix issue

--- iotcontractminimalsample/main.go
+++ iotcontractminimalsample/main.go
@@ -20,7 +20,7 @@ package main
 
 import (
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	iot "github.com/bluemix-git/chaincode/iotcontractplatform"
+	iot "github.com/ibm-watson-iot/blockchain-samples/contracts/platform/iotcontractplatform"
 )
 
 // Update the path to match your configuration
